f641b80fd5a900797b9142ac9ae6f36e63f2812a
LoiKos <LoiKos@users.noreply.github.com>
2022-06-20 11:28:33 +0200

    chore: cleanup after Development

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,5 +1,4 @@
 import _ from 'lodash';
-import printMe from './print.js';
 
  function component() {
    const element = document.createElement('div');
@@ -8,7 +7,6 @@ import printMe from './print.js';
    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   btn.innerHTML = 'Click me and you may check the console!';
-  btn.onclick = printMe;
 
   element.appendChild(btn);
 
