6f06af25d518cb3d5d8cdfe2dedcb5388253856e
LoiKos <LoiKos@users.noreply.github.com>
2022-06-20 11:31:27 +0200

    chore: cleanup config

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -5,24 +5,12 @@ module.exports = {
   mode:'development',
   entry: {
     index: './src/index.js',
-    print: './src/print.js',
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    }),
-  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, "dist"),
     clean:true,
     publicPath: '/',
   },
-  optimization: {
-    runtimeChunk: 'single',
-  },
+
 };
